4c6110c11a23e2040540720a34bf28ce70e8d73d
SOLARIS <jinhoyang@snu.ac.kr>
2018-08-19 01:07:52 +0900

    ch11 change indent 4 to 2

--- Ch11-GAN/mnist_gan.py
+++ Ch11-GAN/mnist_gan.py
@@ -14,17 +14,18 @@ mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
 # 생성된 MNIST 이미지를 8x8 Grid로 보여주는 plot 함수를 정의합니다.
 def plot(samples):
-    fig = plt.figure(figsize=(8, 8))
-    gs = gridspec.GridSpec(8, 8)
-    gs.update(wspace=0.05, hspace=0.05)
+  fig = plt.figure(figsize=(8, 8))
+  gs = gridspec.GridSpec(8, 8)
+  gs.update(wspace=0.05, hspace=0.05)
 
-    for i, sample in enumerate(samples):
-        ax = plt.subplot(gs[i])
-        plt.axis('off')
-        plt.imshow(sample.reshape(28, 28))
-    return fig
+  for i, sample in enumerate(samples):
+    ax = plt.subplot(gs[i])
+    plt.axis('off')
+    plt.imshow(sample.reshape(28, 28))
 
-# 설정값들을 선언합니다.
+  return fig
+
+# 학습에 필요한 설정값들을 선언합니다.
 num_epoch = 100000
 batch_size = 64
 num_input = 28 * 28
@@ -39,22 +40,22 @@ z = tf.placeholder(tf.float32, shape=[None, num_latent_variable])     # 인풋 L
 # Generator 변수들을 설정합니다. 
 # 100 -> 128 -> 784
 with tf.variable_scope('generator'):
-    # 히든 레이어 파라미터 
-    G_W1 = tf.Variable(tf.random_normal(shape=[num_latent_variable, num_hidden], stddev=5e-2))   
-    G_b1 = tf.Variable(tf.constant(0.1, shape=[num_hidden]))
-    # 아웃풋 레이어 파라미터
-    G_W2 = tf.Variable(tf.random_normal(shape=[num_hidden, num_input], stddev=5e-2))   
-    G_b2 = tf.Variable(tf.constant(0.1, shape=[num_input]))
+  # 히든 레이어 파라미터 
+  G_W1 = tf.Variable(tf.random_normal(shape=[num_latent_variable, num_hidden], stddev=5e-2))   
+  G_b1 = tf.Variable(tf.constant(0.1, shape=[num_hidden]))
+  # 아웃풋 레이어 파라미터
+  G_W2 = tf.Variable(tf.random_normal(shape=[num_hidden, num_input], stddev=5e-2))   
+  G_b2 = tf.Variable(tf.constant(0.1, shape=[num_input]))
 
 # Discriminator 변수들을 설정합니다.
 # 784 -> 128 -> 1
 with tf.variable_scope('discriminator'):
-    # 히든 레이어 파라미터
-    D_W1 = tf.Variable(tf.random_normal(shape=[num_input, num_hidden], stddev=5e-2))   
-    D_b1 = tf.Variable(tf.constant(0.1, shape=[num_hidden]))
-    # 아웃풋 레이어 파라미터
-    D_W2 = tf.Variable(tf.random_normal(shape=[num_hidden, 1], stddev=5e-2))   
-    D_b2 = tf.Variable(tf.constant(0.1, shape=[1]))
+  # 히든 레이어 파라미터
+  D_W1 = tf.Variable(tf.random_normal(shape=[num_input, num_hidden], stddev=5e-2))   
+  D_b1 = tf.Variable(tf.constant(0.1, shape=[num_hidden]))
+  # 아웃풋 레이어 파라미터
+  D_W2 = tf.Variable(tf.random_normal(shape=[num_hidden, 1], stddev=5e-2))   
+  D_b2 = tf.Variable(tf.constant(0.1, shape=[1]))
 
 # Generator를 생성하는 함수를 정의합니다.
 # Inputs:
@@ -62,11 +63,11 @@ with tf.variable_scope('discriminator'):
 # Outputs:
 #   generated_mnist_image : 생성된 MNIST 이미지
 def build_generator(X):
-    hidden_layer = tf.nn.relu((tf.matmul(X, G_W1) + G_b1))
-    output_layer = tf.matmul(hidden_layer, G_W2) + G_b2
-    generated_mnist_image = tf.nn.sigmoid(output_layer)
+  hidden_layer = tf.nn.relu((tf.matmul(X, G_W1) + G_b1))
+  output_layer = tf.matmul(hidden_layer, G_W2) + G_b2
+  generated_mnist_image = tf.nn.sigmoid(output_layer)
 
-    return generated_mnist_image
+  return generated_mnist_image
 
 # Discriminator를 생성하는 함수를 정의합니다.
 # Inputs:
@@ -75,11 +76,11 @@ def build_generator(X):
 #   predicted_value : Discriminator가 판단한 True(1) or Fake(0)
 #   logits : sigmoid를 씌우기전의 출력값
 def build_discriminator(X):
-    hidden_layer = tf.nn.relu((tf.matmul(X, D_W1) + D_b1))
-    logits = tf.matmul(hidden_layer, D_W2) + D_b2
-    predicted_value = tf.nn.sigmoid(logits)
+  hidden_layer = tf.nn.relu((tf.matmul(X, D_W1) + D_b1))
+  logits = tf.matmul(hidden_layer, D_W2) + D_b2
+  predicted_value = tf.nn.sigmoid(logits)
 
-    return predicted_value, logits
+  return predicted_value, logits
 
 # 생성자(Generator)를 선언합니다.
 G = build_generator(z)
@@ -101,41 +102,41 @@ tvar = tf.trainable_variables()
 dvar = [var for var in tvar if 'discriminator' in var.name]
 gvar = [var for var in tvar if 'generator' in var.name]
 
-# Discriminator와 Generator의 Optimizer를 정의합니다.
+# Discriminator와 Generator를 위한 Optimizer들을 정의합니다.
 d_train_step = tf.train.AdamOptimizer(learning_rate).minimize(d_loss, var_list=dvar)
 g_train_step = tf.train.AdamOptimizer(learning_rate).minimize(g_loss, var_list=gvar)
 
 # 생성된 이미지들을 저장할 generated_output 폴더를 생성합니다.
 num_img = 0
 if not os.path.exists('generated_output/'):
-    os.makedirs('generated_output/')
+  os.makedirs('generated_output/')
 
 # 그래프를 실행합니다.
 with tf.Session() as sess:
-    # 변수들에 초기값을 할당합니다.
-    sess.run(tf.global_variables_initializer())
+  # 변수들에 초기값을 할당합니다.
+  sess.run(tf.global_variables_initializer())
 
-    # num_epoch 횟수만큼 최적화를 수행합니다.
-    for i in range(num_epoch):
-        # MNIST 이미지를 batch_size만큼 불러옵니다. 
-        batch_X, _ = mnist.train.next_batch(batch_size)
-        # Latent Variable의 인풋으로 사용할 noise를 Uniform Distribution에서 batch_size 개수만큼 샘플링합니다.
-        batch_noise = np.random.uniform(-1., 1., [batch_size, 100])
+  # num_epoch 횟수만큼 최적화를 수행합니다.
+  for i in range(num_epoch):
+    # MNIST 이미지를 batch_size만큼 불러옵니다. 
+    batch_X, _ = mnist.train.next_batch(batch_size)
+    # Latent Variable의 인풋으로 사용할 noise를 Uniform Distribution에서 batch_size 개수만큼 샘플링합니다.
+    batch_noise = np.random.uniform(-1., 1., [batch_size, 100])
 
-        # 500번 반복할때마다 생성된 이미지를 저장합니다.
-        if i % 500 == 0:
-            samples = sess.run(G, feed_dict={z: np.random.uniform(-1., 1., [64, 100])})
-            fig = plot(samples)
-            plt.savefig('generated_output/%s.png' % str(num_img).zfill(3), bbox_inches='tight')
-            num_img += 1
-            plt.close(fig)
+    # 500번 반복할때마다 생성된 이미지를 저장합니다.
+    if i % 500 == 0:
+      samples = sess.run(G, feed_dict={z: np.random.uniform(-1., 1., [64, 100])})
+      fig = plot(samples)
+      plt.savefig('generated_output/%s.png' % str(num_img).zfill(3), bbox_inches='tight')
+      num_img += 1
+      plt.close(fig)
 
-        # Discriminator 최적화를 수행하고 Discriminator의 손실함수를 return합니다.
-        _, d_loss_print = sess.run([d_train_step, d_loss], feed_dict={X: batch_X, z: batch_noise})
+    # Discriminator 최적화를 수행하고 Discriminator의 손실함수를 return합니다.
+    _, d_loss_print = sess.run([d_train_step, d_loss], feed_dict={X: batch_X, z: batch_noise})
 
-        # Generator 최적화를 수행하고 Generator 손실함수를 return합니다.
-        _, g_loss_print = sess.run([g_train_step, g_loss], feed_dict={z: batch_noise})
+    # Generator 최적화를 수행하고 Generator 손실함수를 return합니다.
+    _, g_loss_print = sess.run([g_train_step, g_loss], feed_dict={z: batch_noise})
 
-        # 100번 반복할때마다 Discriminator의 손실함수와 Generator 손실함수를 출력합니다.
-        if i % 100 == 0:
-            print('반복(Epoch): %d, Generator 손실함수(g_loss): %f, Discriminator 손실함수(d_loss): %f' % (i, g_loss_print, d_loss_print))
+    # 100번 반복할때마다 Discriminator의 손실함수와 Generator 손실함수를 출력합니다.
+    if i % 100 == 0:
+      print('반복(Epoch): %d, Generator 손실함수(g_loss): %f, Discriminator 손실함수(d_loss): %f' % (i, g_loss_print, d_loss_print))
